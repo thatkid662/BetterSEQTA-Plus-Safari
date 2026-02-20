02c2fbc8f8e391d0833cd2119cf3dbefb527c6f1
jjhvjhv <arastineyt@gmail.com>
2026-02-20 12:14:26 +0800

    Add files via upload

---
+++ BetterSeqtaPlus/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Anka on 2/20/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
